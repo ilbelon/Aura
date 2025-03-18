6f7ffd1a78d830b8d94eab5b364b1136f5c5d18c
anedra <anedraemme@gmail.com>
2025-03-18 09:20:49 +0100

    clean files

--- Source/Aura/Private/AuraCharacterBase.cpp
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -3,12 +3,9 @@
 
 #include "AuraCharacterBase.h"
 
-// Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned r
@@ -17,18 +14,3 @@ void AAuraCharacterBase::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void AAuraCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
